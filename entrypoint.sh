16ba3735d5782edf50302f1b6085cd3b0e5ce6fd
djalal <hi@djal.al>
2021-11-01 15:47:29 +0100

    implement script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -e
 
-#api_url="https://pokeapi.co/api/v2/pokemon/${INPUT_POKEMON_ID}"
-api_url="https://pokeapi.co/api/v2/pokemon/${INPUT_CLOUDINARY_URL}"
-echo $api_url
+echo CLOUDINARY_URL=$INPUT_CLOUDINARY_URL
+echo LEFT_TEXT=$INPUT_LEFT_TEXT
+echo RIGHT_TEXT=$INPUT_RIGHT_TEXT
+echo COLOR=$INPUT_COLOR
+echo PUBLIC_ID=$INPUT_PUBLIC_ID
 
-pokemon_name=$(curl "${api_url}" | jq ".name")
-echo $pokemon_name
+python -V
+pip3 install cloudinary-cli
 
-echo "::set-output name=label::$pokemon_name"
+BADGE_URI=${LEFT_TEXT}-${RIGHT_TEXT}-${COLOR}
+
+cloudinary uploader upload https://img.shields.io/badge/$BADGE_URI public_id=$PUBLIC_ID resource_type=image > result.json
+
+OUTPUT_PUBLIC_ID=`cat result.json  | jq ".public_id"`
+OUTPUT_SECURE_URL=`cat result.json  | jq ".secure_url"`
+OUTPUT_BYTES=`cat result.json  | jq ".bytes"`
+
+echo "::set-output name=public_id::$OUTPUT_PUBLIC_ID"
+echo "::set-output name=secure_url::$OUTPUT_SECURE_URL"
+echo "::set-output name=bytes::$OUTPUT_BYTES"
