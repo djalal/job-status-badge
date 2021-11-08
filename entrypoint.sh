2942120f6a7f08becc3fdb8eadea4a3ab0483c9f
djalal <djalal@users.noreply.github.com>
2021-11-08 10:31:58 +0100

    force invalidation

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ BADGE_URI=${INPUT_LEFT_TEXT}-${INPUT_RIGHT_TEXT}-${INPUT_COLOR}
 export CLOUDINARY_URL=$INPUT_CLOUDINARY_URL
 
 cloudinary config
-cloudinary uploader upload https://img.shields.io/badge/$BADGE_URI public_id=$INPUT_PUBLIC_ID resource_type=image > result.json
+cloudinary uploader upload https://img.shields.io/badge/$BADGE_URI public_id=$INPUT_PUBLIC_ID invalidate=True resource_type=image > result.json
 
 OUTPUT_PUBLIC_ID=`cat result.json  | jq ".public_id"`
 OUTPUT_SECURE_URL=`cat result.json  | jq ".secure_url"`
